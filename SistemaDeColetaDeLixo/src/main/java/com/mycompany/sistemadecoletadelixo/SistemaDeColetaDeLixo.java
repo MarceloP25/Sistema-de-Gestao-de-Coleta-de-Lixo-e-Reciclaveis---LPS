fe491b277fb90e5550f5e4da5a26f5756062c677
Marcelo P. Patrício <marc3lo.patricio.25@gmail.com>
2024-11-10 17:40:41 -0300

    Início das views

---
+++ SistemaDeColetaDeLixo/src/main/java/com/mycompany/sistemadecoletadelixo/SistemaDeColetaDeLixo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadecoletadelixo;
+
+/**
+ *
+ * @author marce
+ */
+public class SistemaDeColetaDeLixo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
