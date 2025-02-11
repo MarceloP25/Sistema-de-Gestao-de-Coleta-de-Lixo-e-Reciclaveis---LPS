b7eb93dfcfccf3b578f814f649d9ce2ad7dada5f
Marcelo P. Patrício <marc3lo.patricio.25@gmail.com>
2025-02-10 23:07:57 -0300

    Inicio da camada DAO (sprint 6)

--- SistemaDeColetaDeLixo/src/main/java/com/mycompany/sistemadecoletadelixo/adminsystem/model/DAO/CidadaoDAO.java
+++ SistemaDeColetaDeLixo/src/main/java/com/mycompany/sistemadecoletadelixo/adminsystem/model/DAO/CidadaoDAO.java
@@ -8,6 +8,110 @@ package com.mycompany.sistemadecoletadelixo.adminsystem.model.DAO;
  *
  * @author marce
  */
-public class CidadaoDAO {
+import com.mycompany.sistemadecoletadelixo.adminsystem.factory.DatabaseJPA;
+import com.mycompany.sistemadecoletadelixo.adminsystem.model.entities.Cidadao;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+
+
+public class CidadaoDAO implements IDAO<Cidadao> {
+
+    private EntityManager entityManager;
     
+    @Override
+    public void save(Cidadao obj) { 
+        this.entityManager = DatabaseJPA.getInstance().getEntityManager();
+        this.entityManager.getTransaction().begin();
+        this.entityManager.persist(obj);
+        this.entityManager.getTransaction().commit();
+        this.entityManager.close();
+    }
+
+    @Override
+    public void update(Cidadao obj) {  
+        this.entityManager = DatabaseJPA.getInstance().getEntityManager();
+        this.entityManager.getTransaction().begin();
+        this.entityManager.merge(obj);
+        this.entityManager.getTransaction().commit();
+        this.entityManager.close();
+    }
+
+    @Override
+    public Cidadao find(Long id) {
+        this.entityManager = DatabaseJPA.getInstance().getEntityManager();
+
+        Cidadao cidadao = this.entityManager.find(Cidadao.class, id);
+
+        this.entityManager.close();
+
+        return cidadao;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        this.entityManager = DatabaseJPA.getInstance().getEntityManager();
+        this.entityManager.getTransaction().begin();
+
+        Cidadao cidadaoJPA = this.entityManager.find(Cidadao.class, id);
+        if (cidadaoJPA != null) {
+            this.entityManager.remove(cidadaoJPA);
+        } else {
+            this.entityManager.getTransaction().rollback();
+            throw new CidadaoException("Error - Aluno inexistente.");
+        }
+
+        this.entityManager.getTransaction().commit();
+        this.entityManager.close();
+        return true;
+    }
+
+    @Override
+    public List<Cidadao> findAll() {
+        this.entityManager = DatabaseJPA.getInstance().getEntityManager();
+
+        String jpql = "SELECT c FROM Cidadao c";
+        TypedQuery qry = this.entityManager.createQuery(jpql, Cidadao.class);
+
+        List<Cidadao> lst = qry.getResultList();
+
+        this.entityManager.close();
+        return lst;
+    }
+
+    public Cidadao findByCpf(String cpf) {
+        this.entityManager = DatabaseJPA.getInstance().getEntityManager();
+
+        String jpql = " SELECT c "
+                + " FROM Cidadao c "
+                + " WHERE c.cpf like :cpf";
+        TypedQuery<Cidadao> qry = this.entityManager.createQuery(jpql, Cidadao.class);
+        qry.setParameter("cpf", cpf);
+
+        List lst = qry.getResultList();
+        this.entityManager.close();
+
+        if (lst.isEmpty()) {
+            return null;
+        } else {
+            return (Cidadao) lst.get(0);
+        }
+    }
+
+    public List<Cidadao> filterByName(String nome) {
+        this.entityManager = DatabaseJPA.getInstance().getEntityManager();
+
+        String jpql = " SELECT c "
+                + " FROM Cidadao c "
+                + " WHERE c.nome like :nome ";
+        TypedQuery<Cidadao> qry = this.entityManager.createQuery(jpql, Cidadao.class);
+        qry.setParameter("nome", nome + "%");
+
+        List<Cidadao> lst = qry.getResultList();
+
+        this.entityManager.close();
+        return lst;
+    }
+
 }
